b4e924eb32d5955255532ed21e7d7778432c1a46
domenico-simone <dome.simone@gmail.com>
2024-01-19 01:11:20 +0100

    sample data generation: logging&options [update]

--- utils/generate_sample_data.py
+++ utils/generate_sample_data.py
@@ -1,12 +1,26 @@
 import argparse
+import logging
 import os
 import pandas as pd
 import random
 import string
 from datetime import datetime, timedelta
 
-def generate_sample_data_hourly(n_events: int = 100000, n_banners: int = 10,
-                                n_pages: int = 20, start_time: datetime = datetime.utcnow()):
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(funcName)s - %(message)s",
+    level=logging.DEBUG,
+)
+logger = logging.getLogger(__name__)
+
+n_events_default  = 100000
+n_banners_default = 10
+n_pages_default   = 20
+n_batches_default = 10
+
+def generate_sample_data_hourly(n_events: int = n_events_default, 
+                                n_banners: int = n_banners_default,
+                                n_pages: int = n_pages_default, 
+                                start_time: datetime = datetime.utcnow()):
     # event types:
     # - 0 is view
     # - 1 is click
@@ -35,6 +49,24 @@ def generate_sample_data_hourly(n_events: int = 100000, n_banners: int = 10,
     return pd.DataFrame(data)
 
 if __name__ == "__main__":
+
+    # parse command line args
+    parser = argparse.ArgumentParser(
+                    prog='Ad sample data generator',
+                    description='Generate a table with 1hr of ad sample data.')
+
+    parser.add_argument('-e', '--n-events', default=n_events_default,
+                        help="Number of events to be generated")
+    parser.add_argument('-b', '--n-banners', default=n_banners_default,
+                        help="Number of banners to be generated")
+    parser.add_argument('-p', '--n-pages', default=n_pages_default,
+                        help="Number of pages to be generated")
+    parser.add_argument('-t', '--n-batches', default=n_batches_default,
+                        help="Number of data batches to be generated")
+        
+    args = parser.parse_args()
+    # print(args.filename, args.count, args.verbose)
+    
     # Generate appropriate out folder
     out_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../data/sample_data")
     os.makedirs(out_folder, exist_ok=True)
@@ -42,9 +74,21 @@ if __name__ == "__main__":
     # Generate 10 hourly batches of data
     start_time = datetime.utcnow()
 
-    for i in range(10):
-        print(f"Generating batch n. {i+1} starting from timestamp {start_time}")
-        sample_data = generate_sample_data_hourly(n_events=200000, start_time=start_time)
+    if args.n_batches:
+        n_batches = args.n_batches
+    else:
+        n_batches = n_batches_default
+
+    logging.info(f"Ad sample data generation params:") 
+    logging.info(f"n_events={args.n_events}")
+    logging.info(f"n_banners={args.n_banners}")
+    logging.info(f"n_pages={args.n_pages}")
+    logging.info(f"n_batches={n_batches}")
+
+    for i in range(n_batches):
+        logging.debug(f"Generating batch n. {i+1} starting from timestamp {start_time}")
+        sample_data = generate_sample_data_hourly(n_events=200000,
+                                                  start_time=start_time)
         sample_data.to_csv(os.path.join(out_folder, f'sample_data_0{i+1}.csv'), index=False)
-        print(f'Exported file sample_data_{str(i+1).zfill(2)}.csv')
+        logging.debug(f'Exported file sample_data_{str(i+1).zfill(2)}.csv')
         start_time += timedelta(hours=1)
