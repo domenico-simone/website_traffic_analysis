ad1d251a6b8d47f7fca47e729ec1abc811aa1a0c
domenico-simone <dome.simone@gmail.com>
2024-01-20 17:03:47 +0100

    yaml conf file parsing

---
+++ utils/funcs.py
@@ -0,0 +1,16 @@
+import yaml
+
+def parse_defaults() -> dict:
+    conf = yaml.safe_load(open('defaults.yaml', 'r').read())
+    return conf
+
+def parse_conf(conf_file=None) -> dict:
+    default_conf = parse_defaults()
+    custom_conf = yaml.safe_load(open(conf_file, 'r').read())
+    final_conf = {}
+    for k in default_conf:
+        if k in custom_conf:
+            final_conf[k] = custom_conf[k]
+        else:
+            final_conf[k] = default_conf[k]
+    return final_conf
