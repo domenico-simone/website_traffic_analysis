5b60f13a5e9178e98e3478a9cb58ffc1b477de9e
domenico-simone <dome.simone@gmail.com>
2024-01-20 10:46:52 +0100

    hourly_stats.py: input file option [update]

--- hourly_stats.py
+++ hourly_stats.py
@@ -1,5 +1,29 @@
+import argparse
+import logging
+import os
 import pyspark.sql.functions as F
 from pyspark.sql import SparkSession
+
+
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(funcName)s - %(message)s",
+    level=logging.INFO,
+)
+logger = logging.getLogger(__name__)
+
+# parse command line args
+parser = argparse.ArgumentParser(
+                description='Compute stats for an event batch file.')
+
+parser.add_argument('-i', '--input-file', default="data/sample_data/sample_data_01.csv",
+                    help="Event batch input file (%(default)s)")
+
+args = parser.parse_args()
+
+# Check if input file exists, exit if not
+if not os.path.isfile(args.input_file):
+    logging.error(f"Input file {args.input_file} not found!") 
+    raise FileNotFoundError
 
 spark = SparkSession.builder.appName("PlacementStats").getOrCreate()
 
