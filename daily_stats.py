e46c5a281d98efcccd84df1cc6e4a86c4de8c261
domenico-simone <dome.simone@gmail.com>
2024-01-24 01:28:10 +0100

    daily_stats [update]

--- daily_stats.py
+++ daily_stats.py
@@ -11,24 +11,42 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-spark = SparkSession.builder \
-        .appName("WebtrafficStats_daily") \
-        .master("local[2]").getOrCreate()
-
-# Load data
-df_all = spark.read.csv("data/sample_data/", header=True, inferSchema=True)
-
-# Daily statistics for the requested grouping id
-daily_stats = (
-    df_all.groupBy("page_id")
-    .agg(
-        F.date_format(F.from_unixtime(F.any_value("timestamp")), "yyyy-MM-dd").alias('date'),
-        F.count(F.when(F.col("event_type") == 0, 1)).alias("n_views"),
-        F.sum("event_type").alias("n_clicks"),
-        F.countDistinct("user_id").alias("distinct_users")
+def get_daily_stats(df_all, grouping_field):
+    daily_stats = (
+        df_all.groupBy(grouping_field)
+        .agg(
+            F.date_format(F.from_unixtime(F.any_value("timestamp")), "yyyy-MM-dd").alias('date'),
+            F.count(F.when(F.col("event_type") == 0, 1)).alias("n_views"),
+            F.sum("event_type").alias("n_clicks"),
+            F.countDistinct("user_id").alias("distinct_users")
+        )
     )
-)
 
-daily_stats.show()
+    return daily_stats
 
-spark.stop()
+if __name__ == "__main__":
+
+    # parse command line args
+    parser = argparse.ArgumentParser(
+                    description='Compute stats for an hourly event batch file.')
+
+    parser.add_argument('-i', '--input-folder', default="data/sample_data/",
+                        help="Folder with event batch input files (default: %(default)s)")
+    parser.add_argument('-g', '--grouping-field', default="placement_id",
+                        help="Event table field to compute statistics for (default: %(default)s)")
+
+    args = parser.parse_args()
+    input_folder = args.input_folder
+    spark = SparkSession.builder \
+            .appName("WebtrafficStats_daily") \
+            .master("local[2]").getOrCreate()
+
+    # Load data
+    df_all = spark.read.csv(input_folder, header=True, inferSchema=True)
+
+    # Daily statistics for the requested grouping id
+    daily_stats = get_daily_stats(df_all, args.grouping_field)
+
+    daily_stats.show()
+
+    spark.stop()
