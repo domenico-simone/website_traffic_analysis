6b1df7214dcee3cd80bd158e67a7f6832f6a600c
domenico-simone <dome.simone@gmail.com>
2024-01-24 08:40:33 +0100

    DbLogger class [update]

--- utils/schemas.py
+++ utils/schemas.py
@@ -1,3 +1,4 @@
+import json
 from pyspark.sql.types import StructType, StringType, StructField, IntegerType
 
 event_schema = StructType([
@@ -8,3 +9,14 @@ event_schema = StructType([
     StructField("page_id", StringType(), True),
     StructField("user_id", StringType(), True),
 ])
+
+class DbLogger:
+    def __init__(self, status, message, timestamp, batch_type, datetime_log):
+        self.status = status
+        self.message = message
+        self.timestamp = timestamp
+        self.batch_type = batch_type
+        self.datetime = datetime_log
+    
+    def __str__(self):
+        return json.dumps(vars(self))
